799ccced1c923d12adcb9b75d4ebab67a7c176f6
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2014-03-18 18:42:25 -0300

    codigo desnecessario removido

--- models/models.py
+++ models/models.py
@@ -3,7 +3,3 @@ from google.appengine.ext import db
 # These classes define the data objects
 # that you will be able to store in
 # AppEngine's data store.
-
-class ApiCredential(db.Model):
-	api_cookie = db.StringProperty(required = True)
-	date = db.DateTimeProperty(auto_now_add=True)
